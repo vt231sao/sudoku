8b3bee95a75ab2a40718fd746feb779cd1e63eb6
Andrii Soroka <vt231_sao@student.ztu.edu.ua>
2025-10-29 04:27:10 +0200

    Lab-02 2.Інтегрування хука гри судоку в компоненти

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
-import SudokuBoard from '../components/SudokuBoard/SudokuBoard';
-import Button from '../components/Button.jsx';
-
+import React, { useEffect } from 'react';
+import SudokuBoard from '../components/SudokuBoaard/SudokuBoard';
+import Button from '../components/Button';
+import { useSudokuGame } from '../hooks/useSudokuGame';
 function GamePage(props) {
+    const {
+        board,
+        initialBoard,
+        selectedCell,
+        selectCell,
+        handleInput
+    } = useSudokuGame();
+
+    useEffect(() => {
+        function handleKeyDown(event) {
+            const { key } = event;
+            if (key >= '1' && key <= '9') {
+                handleInput(parseInt(key, 10));
+            } else if (key === 'Backspace' || key === 'Delete') {
+                handleInput(null);
+            }
+        }
+
+        window.addEventListener('keydown', handleKeyDown);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [handleInput]);
+
+
     function handleBack() {
         props.onBack();
     }
@@ -17,7 +42,12 @@ function GamePage(props) {
                 React.createElement(Button, { onClick: handleBack }, 'Назад'),
                 React.createElement(Button, { onClick: handleFinishClick }, 'Закінчити')
             ),
-            React.createElement(SudokuBoard, null)
+            React.createElement(SudokuBoard, {
+                board: board,
+                initialBoard: initialBoard,
+                selectedCell: selectedCell,
+                onCellClick: selectCell
+            })
         )
     );
 }
