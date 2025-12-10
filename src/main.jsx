8e681272247fb0c3514fe6487f3452a20e493f41
Andrii Soroka <vt231_sao@student.ztu.edu.ua>
2025-12-10 04:13:16 +0200

    Додано сторінку таблиці лідерів, оновлено роутинг та видалено SettingsContext

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles/globals.css';
-import { SettingsProvider } from './context/SettingsContext';
 
 createRoot(document.getElementById('root')).render(
     React.createElement(BrowserRouter, null,
-        React.createElement(SettingsProvider, null,
-            React.createElement(App, null)
-        )
+        React.createElement(App, null)
     )
 );
